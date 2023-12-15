b9d896c188692d52bcdafd23754985db40021610
Artur Sadykov <artursadykov31.10@gmail.com>
2023-12-15 13:07:42 +0300

    1

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace sf;
 int main()
 {
 
-    RenderWindow window(VideoMode(400, 400), L"Новый проект", Style::Default);
+    RenderWindow window(VideoMode(400, 400), L"Labirinth", Style::Default);
 
     window.setVerticalSyncEnabled(true);
 
